17a72389ffe5713d58a9833fe6fa5ec4c08dff5c
xulang <176779597@qq.com>
2020-02-29 16:35:57 +0800

    发布版2

--- 合并视频.py
+++ 合并视频.py
@@ -1,12 +1,16 @@
 from moviepy.editor import *
 import os
 L = []
-for root, dirs, files in os.walk("./video"):
+for root, dirs, files in os.walk("./download"):
     # 按文件名排序
     files.sort()
     # 遍历所有文件
+    print(root)#输出母目录
+    print(files)#输出文件列表
+    print(dirs)#输出子目录
     for file in files:
         # 如果后缀名为 .mp4
+        print(file)
         if os.path.splitext(file)[1] == '.mp4':
             # 拼接成完整路径
             filePath = os.path.join(root, file)
