8a2e4c2ec39d59d3df5c11151412830a8679a350
xulang <176779597@qq.com>
2020-03-04 00:28:10 +0800

    20200304增加导入excel

--- 多线程爬取adultdouga.py
+++ 多线程爬取adultdouga.py
@@ -4,10 +4,10 @@ import urllib3
 import threading
 import re
 from queue import Queue
-
+import csv
 
 class ThreadNum(threading.Thread):
-    def __init__(self, q, q1):
+    def __init__(self, q,q1):
         threading.Thread.__init__(self)
         self.q = q
         self.q1 = q1
@@ -27,10 +27,8 @@ class ThreadNum(threading.Thread):
                     continue
             comment = re.compile(r'</td><td(.*?)</div></div>', re.DOTALL)
             class_list = comment.findall(page_str)
-            n = 0
             for line in class_list:
                 try:
-                    n = n + 1
                     httpAdr_tmp = re.findall('.href=\'(.*?)\'"><',line)[0]
                     httpAdr = httpAdr_tmp.replace('./','https://adultdouga.jp/')
                     r1 = http.request('GET',httpAdr+"/get")
@@ -39,7 +37,9 @@ class ThreadNum(threading.Thread):
                     av_mp4 = re.findall('<video src="(.*?)"',class_page_str)[0]
                     av_date = re.findall('\d{4}-\d{2}-\d{2}',line)[0]
                     print(av_name+ '爬取完成')
-                    self.q1.put([av_name,av_date,httpAdr,av_mp4])
+                    tmpList = [av_name,av_date,httpAdr,av_mp4]
+                    self.q1.put(tmpList)
+                    print(q1.qsize())
                 except Exception as e:
                     continue
 
@@ -47,14 +47,29 @@ class ThreadNum(threading.Thread):
 if __name__ == '__main__':
     q = Queue()
     q1 = Queue()
+    ThreadList = []
     for n in range(1, 7):
         q.put('https://adultdouga.jp/?search=&page=%d&sort=date' % n)
+    list1 = []
     for n in range(5):
         n = ThreadNum(q,q1)
+        list1.append(n)
+    for n in list1:
         n.start()
+    for n in list1:
+        n.join()
+    endList = []
+    print(q1.qsize())
+    while not q1.empty():
+        endList.append(q1.get())
+    import xlrd
+    import xlwt
+    from xlutils.copy import copy
+    index = len(endList)
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet('1')
+    for i in range(0, index):
+        for j in range(0, len(endList[i])):
+            sheet.write(i, j, endList[i][j])
+    workbook.save('工作簿.xls')
 
-
-
-
-#r = requests.get(page_adr, headers={'Host':'adultdouga.jp','Connection':'keep-alive','Cache-Control':'max-age=0','Upgrade-Insecure-Requests':'1','User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36','Sec-Fetch-Dest':'document','Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9','Sec-Fetch-Site':'same-origin','Sec-Fetch-Mode':'navigate','Sec-Fetch-User':'?1','Referer':'https://adultdouga.jp/','Accept-Encoding':'gzip, deflate, br','Accept-Language':'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7'})
-#print(r.text)
